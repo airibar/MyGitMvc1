bbd93fe69f4dc97f362084705559c8668005a26b
AlvaroI <airibar2000@gmail.com>
2021-03-12 16:21:47 -0500

    Setting up Local Branch

---
+++ MiGitMvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiGitMvc1.Startup))]
+namespace MiGitMvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
