21dbe87178572f445f34412243ee33603c97be66
AlvaroI <airibar2000@gmail.com>
2021-03-12 17:45:04 -0500

    Change Main Menu APP Name and Route to Index Task

---
+++ MiGitMvc1/Controllers/TheTaskController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiGitMvc1.Controllers
+{
+    public class TheTaskController : Controller
+    {
+        // GET: Task
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
