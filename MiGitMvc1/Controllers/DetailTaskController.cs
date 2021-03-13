f16ba616b872977184fba35f48c9005f475ffdc3
AlvaroI <airibar2000@gmail.com>
2021-03-13 15:03:15 -0500

    Add Button to enter task, plus detail task view with some validation

---
+++ MiGitMvc1/Controllers/DetailTaskController.cs
@@ -0,0 +1,18 @@
+﻿using MiGitMvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiGitMvc1.Controllers
+{
+    public class DetailTaskController : Controller
+    {
+        // GET: DetailTask
+        public ActionResult TaskDetail(OneTask oneTask)
+        {
+            return View(oneTask);
+        }
+    }
+}
