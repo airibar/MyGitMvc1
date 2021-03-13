0b12157bc72eaaea065810b7c88569fe889e62b6
AlvaroI <airibar2000@gmail.com>
2021-03-13 15:59:55 -0500

    Enabling Migrations

---
+++ MiGitMvc1/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MiGitMvc1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MiGitMvc1.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MiGitMvc1.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
