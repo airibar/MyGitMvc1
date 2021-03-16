6dce099f40f00f2d85b8ef926652d7da42e72267
AlvaroI <airibar2000@gmail.com>
2021-03-16 19:27:13 -0400

    Adding DatePicker to Date due page BootStrap and Jquery

--- MiGitMvc1/App_Start/BundleConfig.cs
+++ MiGitMvc1/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace MiGitMvc1
         public static void RegisterBundles(BundleCollection bundles)
             {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,10 +21,12 @@ namespace MiGitMvc1
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                                    "~/Scripts/bootstrap.bundle.js",
+                                    "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-Cyborg.css",
+                      "~/Content/jquery-ui.min.css",
                       "~/Content/site.css"));
             }
         }
---
+++ MiGitMvc1/Migrations/202103161607351_Adding_DateCreation_Field1.cs
@@ -0,0 +1,18 @@
+namespace MiGitMvc1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Adding_DateCreation_Field1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OneTasks", "DateCreation", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OneTasks", "DateCreation");
+        }
+    }
+}
